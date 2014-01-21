4cf86b872df8a68c412e6ae460d0ad52fe1425ac
Mitch <wabbitsnot@gmail.com>
2014-01-21 15:10:43 -0800

    fixed something with the api

--- app/helpers/github_api.rb
+++ app/helpers/github_api.rb
@@ -5,26 +5,25 @@ module GithubApi
   ## need to store sensetive credentials in a yml
   ## need to figure out how to properly pass encoded creds
 
-  username = 'myarchin-sge'
-  password = 'wabbitsnot1'
+  username = 'producerapi-sge'
+  password = 'sl33py69'
+  token = 'ae440e76ea0f5e198039952fd19a80ad915e3dea'
 
   ## List of all Platform repositories
-  plfm_repos = ['commerce_service', 'platform_admin']
-  #plfm_repos = ['VacuumLeek', 'player-portal', 'platform-aws-chef', 'analytics_service', 
-  #              'web-service', 'admin_widgets', 'commons', 'rule_service', 'title_service user_service', 
-  #              'sge-service-template', 'platform_harness', 'platform_docs', 'admin_service', 
-  #              'datum_service', 'auth_service', 'dispatcher-private', 'dispatcher-public harness daemon', 
-  #              'action_worker', 'rules_worker', 'platform_admin', 'commerce_service']
+  plfm_repos = ['commerce_service', 'platform_admin', 'harness', 'title_service', 'commons', 'rule_service', 
+                'user_service', 'admin_widgets', 'dispatcher-private', 'dispatcher-public', 'platform_docs',
+                'admin_service', 'platform_harness', 'datum_service', 'analytics_service']
 
   ## Queries and stores the Platform repositories for the latest version notes in RELEASE.md
   plfm_repos.each do |rpo|
     request = Github.new do |c|
-      c.basic_auth          = "#{username}:#{password}"
-      c.user                = 'myarchin-sge'
+      #c.basic_auth          = "#{username}:#{password}"
+      c.oauth_token         = token
+      c.user                = 'sge'
       c.repo                = rpo
     end
     r = request.repos.contents.get path: 'RELEASE.md', ref: 'develop'
-    PLATFORM_NOTES << "### #{rpo}\n" + Base64.decode64(r[:content]).extract_string("##","##") + "\n"
+    PLATFORM_NOTES << "### #{rpo} -" + Base64.decode64(r[:content]).extract_string("##","##") + "\n"
   end
 
 end
